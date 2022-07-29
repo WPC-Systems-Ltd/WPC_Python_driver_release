c2ca6b3c9df27ff919a59154ac2c456e419e3214
chungleepeople <jinhong2009885@gmail.com>
2022-07-29 12:02:35 +0800

    Add CAN example code

---
+++ Examples/Console/CAN/example_CAN_read.py
@@ -0,0 +1,68 @@
+import asyncio
+import sys
+sys.path.insert(0, 'pywpc/')
+sys.path.insert(0, '../../../pywpc/')
+import pywpc
+
+async def main():
+    print("Start example code...")
+
+    ## Get python driver version
+    print(f'{pywpc.PKG_FULL_NAME} - Version {pywpc.__version__}')
+
+    ## Create device handle
+    dev = pywpc.USBDAQF1CD()
+
+    ## Connect to network device
+    try:
+        dev.connect('21JA1320')
+    except Exception as err:
+        pywpc.printGenericError(err)
+
+    try: 
+        ## Get Firmware model & version
+        driver_info = await dev.Sys_getDriverInfo()
+        print("Model name: " + driver_info[0])
+        print("Firmware version: " + driver_info[-1])
+
+        speed = 0 ## 0 = 125 KHz, 1 = 250 kHz, 2 = 500 kHz, 3 = 1 MHz
+
+        ## CAN Open
+        status = await dev.CAN_open() 
+        if status == 0: print("CAN_open: OK")
+
+        ## Set CAN speed to 0  
+        status = await dev.CAN_setSpeed(speed) 
+        if status == 0: print("CAN_setSpeed: OK")
+
+        ## CAN Start
+        status = await dev.CAN_start() 
+        if status == 0: print("CAN_start: OK")
+
+        ## Sleep for 10 seconds ,meanwhile, you can write data. 
+        await asyncio.sleep(10) ## delay(second)
+
+        ## Read 5 frames
+        data = await dev.CAN_read(5)
+        print(data)
+        
+        ## CAN Stop
+        status = await dev.CAN_stop() 
+        if status == 0: print("CAN_stop: OK")
+        
+        ## CAN Close
+        status = await dev.CAN_close() 
+        if status == 0: print("CAN_close: OK")
+ 
+    except Exception as err:
+        pywpc.printGenericError(err)
+ 
+    ## Disconnect network device
+    dev.disconnect()
+
+    ## Release device handle
+    dev.close()
+    return
+
+if __name__ == '__main__':
+    asyncio.run(main())
