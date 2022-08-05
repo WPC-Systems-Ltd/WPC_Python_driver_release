43bacd70f324b0fad0c8ec850d74a00f37512351
chungleepeople <jinhong2009885@gmail.com>
2022-08-05 17:09:53 +0800

    Update GUI example code with  pywpc version 0.1.16 beta-8

--- Examples/GUI/DIO/Example_digital_output/main.py
+++ Examples/GUI/DIO/Example_digital_output/main.py
@@ -1,5 +1,4 @@
-from calendar import c
-from PyQt5 import QtWidgets, QtGui,QtCore
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import QMessageBox
 from UI_design.Ui_example_GUI_Digital_Output import Ui_MainWindow 
 from qasync import QEventLoop, asyncSlot
@@ -30,7 +29,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.switch_gray_path = current_folder + "/Material/switch_gray.png"
         self.switch_blue_path = current_folder + "/Material/switch_blue.png" 
 
-        ## Set tademark path
+        ## Set trademark path
         self.ui.lb_trademark.setPixmap(QtGui.QPixmap(self.trademark_path))
 
         ## Set led path
@@ -42,54 +41,37 @@ class MainWindow(QtWidgets.QMainWindow):
         ## Define callback events
         self.ui.btn_connect.clicked.connect(self.connectEvent)
         self.ui.btn_disconnect.clicked.connect(self.disconnectEvent)
-        self.ui.comboBox_port.currentIndexChanged.connect(self.portEvent)
+        self.ui.combobox_port.currentIndexChanged.connect(self.portEvent)
 
         for i in range(0, 8):
-            obj_chbox_enb = getattr(self.ui, 'cb_eb%d' %i) 
-            obj_chbox_enb.stateChanged.connect(self.enableDOEvent)
-
             obj_chbox_state = getattr(self.ui, 'cb_state%d' %i) 
             obj_chbox_state.stateChanged.connect(self.stateDOEvent)
             obj_chbox_state.setStyleSheet("QCheckBox::indicator{ width: 60px;height: 60px;} QCheckBox::indicator:unchecked {image: url("+self.switch_gray_path+");} QCheckBox::indicator:checked {image: url("+self.switch_blue_path+");}")
 
-        ## Get Python driver version
+        ## Get Python driver version 
         print(f'{pywpc.PKG_FULL_NAME} - Version {pywpc.__version__}')
+
+    @asyncSlot() 
+    async def OpenDOport(self):
+       for i in range(4):
+        status = await dev.DO_openPort(i)
  
+        await asyncio.sleep(0.1) ## delay(second)
+
+    @asyncSlot() 
+    async def CloseDOport(self):
+       for i in range(4):
+        status = await dev.DO_closePort(i) 
+        print("status",status)
+        await asyncio.sleep(0.1) ## delay(second)
+
     @asyncSlot() 
     async def portEvent(self):
-        # Check connection status
-        if self.checkConnectionStatus() == False:
-            return
-
-        # Get port index from mainUI window
-        port = self.ui.comboBox_port.currentIndex()
-        ## Set DIO to MCU
-        print(self.enable_cal, self.state_cal)
-        await dev.openDOInPins(port, self.enable_cal, self.state_cal)
- 
-
-    @asyncSlot() 
-    async def enableDOEvent(self):
         ## Check connection status
         if self.checkConnectionStatus() == False:
             return
-
-        ## Initialize parameter
-        self.enable_cal = 0 
-
-        ## Get port index from mainUI window
-        port = self.ui.comboBox_port.currentIndex()
-
-        for i in range (8):
-            obj_chbox_enb = getattr(self.ui, 'cb_eb%d' % i) 
-            state = obj_chbox_enb.isChecked()
-            self.enable_cal += int(state) << i
-
-        ## Set DIO to MCU
-        await dev.openDOInPins(port, self.enable_cal, self.state_cal)
  
-
-    @asyncSlot() 
+    @asyncSlot()
     async def stateDOEvent(self):
         ## Check connection status
         if self.checkConnectionStatus() == False:
@@ -98,8 +80,8 @@ class MainWindow(QtWidgets.QMainWindow):
         ## Initialize parameter
         self.state_cal = 0
 
-        ## Get port from MainUI window
-        port = self.ui.comboBox_port.currentIndex()
+        ## Get port from UI
+        port = self.ui.combobox_port.currentIndex()
 
         for i in range (8):
             obj_chbox_state = getattr(self.ui, 'cb_state%d' % i) 
@@ -107,28 +89,35 @@ class MainWindow(QtWidgets.QMainWindow):
             self.state_cal += int(state) << i
 
         ## Set DIO to MCU
-        await dev.openDOInPins(port, self.enable_cal, self.state_cal)
- 
+        await dev.DO_writePort(port, self.state_cal)
 
     @asyncSlot() 
     async def connectEvent(self):
-        # Get ip from MainUI window
-        self.ip = self.ui.lineEdit_ipConnect.text()
+        # Get IP from UI
+        self.IP = self.ui.ld_ip.text()
         try: 
             ## Connect to network device
-            dev.connect(self.ip)
+            dev.connect(self.IP)
 
             ## Change LED status
             self.ui.lb_led.setPixmap(QtGui.QPixmap(self.green_led_path))
             
             ## Change connection flag
             self.connect_flag = 1
+
+            print(dev.DO_getPinMap())
+
+            ## Open DO port
+            self.OpenDOport()
         except pywpc.Error as err:
-            self.ui.lb_err.setText(str(err)) 
+            # self.ui.lb_err.setText(str(err)) 
             print("err: " + str(err))
 
     @asyncSlot()      
     async def disconnectEvent(self):
+        ## Close DO port
+        self.CloseDOport()
+
         ## Disconnect network device
         dev.disconnect() 
         
@@ -144,8 +133,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         ## Release device handle
         dev.close()
- 
-    # Check TCP connection with QMessageBox
+
     def checkConnectionStatus(self):
         if self.connect_flag == 0:
             QMessageBox.information(self, "Error Messages", "Please connect server first.", QMessageBox.Ok)
