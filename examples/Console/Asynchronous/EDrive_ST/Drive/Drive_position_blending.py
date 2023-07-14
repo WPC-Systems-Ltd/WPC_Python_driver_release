edf583003bdba523792f8a96df31497674004ce2
chunglee_people <45706135+chungleepeople@users.noreply.github.com>
2023-07-14 17:54:42 +0800

    v2.1.5 - New product release EDrive-ST (#72)
    v2.1.5 Date: 2023/07/14, Developer: Chunglee_people
    ---------------------------------------------------
    ### Added
    - New product: `EDrive-ST`

---
+++ examples/Console/Asynchronous/EDrive_ST/Drive/Drive_position_blending.py
@@ -0,0 +1,130 @@
+'''
+Drive - Drive_position_blending.py with asynchronous mode.
+
+-------------------------------------------------------------------------------------
+Please change correct serial number or IP and port number BEFORE you run example code.
+
+For other examples please check:
+    https://github.com/WPC-Systems-Ltd/WPC_Python_driver_release/tree/main/examples
+See README.md file to get detailed usage of this example.
+
+Copyright (c) 2023 WPC Systems Ltd. All rights reserved.
+'''
+
+## Python
+import asyncio
+
+## WPC
+
+from wpcsys import pywpc
+
+async def main():
+    ## Get Python driver version
+    print(f'{pywpc.PKG_FULL_NAME} - Version {pywpc.__version__}')
+
+    ## Create device handle
+    dev = pywpc.EDrive_ST()
+
+    ## Connect to device
+    try:
+        dev.connect("192.168.1.110") ## Depend on your device
+    except Exception as err:
+        pywpc.printGenericError(err)
+        ## Release device handle
+        dev.close()
+        return
+
+    try:
+        ## Parameters setting
+        port = 0 ## Depend on your device
+        relative_position_mode = 1
+        target_position = 30000
+        new_position = 0
+        stop_decel = 0
+        timeout = 3  ## second
+
+        ## Polarity and enable parameters
+        active_low = 0
+        active_high = 1
+        en_forward = 0
+        en_reverse = 0
+
+        ## Get firmware model & version
+        driver_info = await dev.Sys_getDriverInfo_async()
+        print("Model name: " + driver_info[0])
+        print("Firmware version: " + driver_info[-1])
+
+        ## EDrive-ST open
+        err = await dev.Drive_open_async(port)
+        print(f"Drive_open: {err}")
+
+        ## EDrive-ST configure
+        err = await dev.Drive_cfgAxisMove_async(port, relative_position_mode, target_position)
+        print(f"Drive_cfgAxisMove: {err}")
+
+        err = await dev.Drive_cfgLimit_async(port, en_forward, en_reverse, active_low)
+        print(f"Drive_cfgLimit: {err}")
+
+        ## EDrive-ST reset
+        err = await dev.Drive_rstEncoderPosi_async(port)
+        print(f"EDST_reset: {err}")
+
+        ## EDrive-ST Servo on
+        err = await dev.Drive_enableServoOn_async(port)
+        print(f"Drive_enableServoOn: {err}")
+
+        ## EDrive-ST start
+        err = await dev.Drive_start_async(port)
+        print(f"Drive_start: {err}")
+
+        ## Wait for 3 seconds for moving
+        await asyncio.sleep(3) ## delay [s]
+
+        ## EDrive-ST read encoder position
+        posi = await dev.Drive_readEncoderPosition_async(port)
+        print(f"encoder_posi: {posi[0]}, logical_posi: {posi[1]}")
+
+        move_status = 0
+        while move_status == 0:
+            move_status = await dev.Drive_getMoveStatus_async(port)
+
+            ## EDrive-ST read encoder position
+            posi = await dev.Drive_readEncoderPosition_async(port)
+            print(f"encoder_posi: {posi[0]}, logical_posi: {posi[1]}")
+
+            ## EDrive-ST overwrite position
+            err = await dev.Drive_overridePosition_async(port, new_position)
+            # print(f"Drive_overridePosition: {err}")
+
+        ## EDrive-ST Stop
+        err = await dev.Drive_stop_async(port, stop_decel)
+        print(f"Drive_stop: {err}")
+
+        ## EDrive-ST Servo off
+        err = await dev.Drive_enableServoOff_async(port)
+        print(f"Drive_enableServoOff: {err}")
+
+        ## EDrive-ST close
+        err = await dev.Drive_close_async(port)
+        print(f"Drive_close: {err}")
+    except Exception as err:
+        pywpc.printGenericError(err)
+
+    ## Disconnect device
+    dev.disconnect()
+
+    ## Release device handle
+    dev.close()
+
+    return
+
+def main_for_spyder(*args):
+    if asyncio.get_event_loop().is_running():
+        return asyncio.create_task(main(*args)).result()
+    else:
+        return asyncio.run(main(*args))
+
+if __name__ == '__main__':
+    asyncio.run(main()) ## Use terminal
+    # await main() ## Use Jupyter or IPython(>=7.0)
+    # main_for_spyder() ## Use Spyder
---
+++ examples/Console/Synchronous/EDrive_ST/Drive/Drive_position_blending.py
@@ -0,0 +1,122 @@
+'''
+Drive - Drive_position_blending.py with synchronous mode.
+
+-------------------------------------------------------------------------------------
+Please change correct serial number or IP and port number BEFORE you run example code.
+
+For other examples please check:
+    https://github.com/WPC-Systems-Ltd/WPC_Python_driver_release/tree/main/examples
+See README.md file to get detailed usage of this example.
+
+Copyright (c) 2023 WPC Systems Ltd. All rights reserved.
+'''
+
+## Python
+import time
+
+## WPC
+
+from wpcsys import pywpc
+
+def main():
+    ## Get Python driver version
+    print(f'{pywpc.PKG_FULL_NAME} - Version {pywpc.__version__}')
+
+    ## Create device handle
+    dev = pywpc.EDrive_ST()
+
+    ## Connect to device
+    try:
+        dev.connect("192.168.1.110") ## Depend on your device
+    except Exception as err:
+        pywpc.printGenericError(err)
+        ## Release device handle
+        dev.close()
+        return
+
+    try:
+        ## Parameters setting
+        port = 0 ## Depend on your device
+        relative_position_mode = 1
+        target_position = 30000
+        new_position = 0
+        stop_decel = 0
+        timeout = 3  ## second
+
+        ## Polarity and enable parameters
+        active_low = 0
+        active_high = 1
+        en_forward = 0
+        en_reverse = 0
+
+        ## Get firmware model & version
+        driver_info = dev.Sys_getDriverInfo(timeout=timeout)
+        print("Model name: " + driver_info[0])
+        print("Firmware version: " + driver_info[-1])
+
+        ## EDrive-ST open
+        err = dev.Drive_open(port, timeout=timeout)
+        print(f"Drive_open: {err}")
+
+        ## EDrive-ST configure
+        err = dev.Drive_cfgAxisMove(port, relative_position_mode, target_position, timeout=timeout)
+        print(f"Drive_cfgAxisMove: {err}")
+
+        err = dev.Drive_cfgLimit(port, en_forward, en_reverse, active_low, timeout=timeout)
+        print(f"Drive_cfgLimit: {err}")
+
+        ## EDrive-ST reset
+        err = dev.Drive_rstEncoderPosi(port, timeout=timeout)
+        print(f"EDST_reset: {err}")
+
+        ## EDrive-ST Servo on
+        err = dev.Drive_enableServoOn(port, timeout=timeout)
+        print(f"Drive_enableServoOn: {err}")
+
+        ## EDrive-ST start
+        err = dev.Drive_start(port, timeout=timeout)
+        print(f"Drive_start: {err}")
+
+        ## Wait for 3 seconds for moving
+        time.sleep(3)
+
+        ## EDrive-ST read encoder position
+        posi = dev.Drive_readEncoderPosition(port, timeout=timeout)
+        print(f"encoder_posi: {posi[0]}, logical_posi: {posi[1]}")
+
+        move_status = 0
+        while move_status == 0:
+            move_status = dev.Drive_getMoveStatus(port, timeout=timeout)
+
+            ## EDrive-ST read encoder position
+            posi = dev.Drive_readEncoderPosition(port, timeout=timeout)
+            print(f"encoder_posi: {posi[0]}, logical_posi: {posi[1]}")
+
+            ## EDrive-ST overwrite position
+            err = dev.Drive_overridePosition(port, new_position, timeout=timeout)
+            # print(f"Drive_overridePosition: {err}")
+
+        ## EDrive-ST Stop
+        err = dev.Drive_stop(port, stop_decel, timeout=timeout)
+        print(f"Drive_stop: {err}")
+
+        ## EDrive-ST Servo off
+        err = dev.Drive_enableServoOff(port, timeout=timeout)
+        print(f"Drive_enableServoOff: {err}")
+
+        ## EDrive-ST close
+        err = dev.Drive_close(port, timeout=timeout)
+        print(f"Drive_close: {err}")
+    except Exception as err:
+        pywpc.printGenericError(err)
+
+    ## Disconnect device
+    dev.disconnect()
+
+    ## Release device handle
+    dev.close()
+
+    return
+
+if __name__ == '__main__':
+    main()
