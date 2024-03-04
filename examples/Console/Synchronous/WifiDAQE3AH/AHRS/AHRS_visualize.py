b52211e43e97ad8a09de7a0010316742a7ca9bdd
thomas.duflos <thomas.duflos@student-cs.fr>
2024-03-04 10:27:32 +0800

    conventions fixes

--- examples/Console/Synchronous/WifiDAQE3AH/AHRS/AHRS_visualize.py
+++ examples/Console/Synchronous/WifiDAQE3AH/AHRS/AHRS_visualize.py
@@ -54,7 +54,6 @@ for font in font_manager.findSystemFonts(DATA_PATH):
 ## Set font family globally
 rcParams['font.family'] = 'Digital-7 Mono'
 
-IMG_WPC = plt.imread('Material/trademark.jpg')
 IMG_WPC_PIL = Image.open('Material/trademark.jpg')
 
 ################################################################################
@@ -87,7 +86,7 @@ PLANE_DICT = {
   'roll': dict(img=IMG_ROLL, size=SIZE_ROLL, center=CENTER_ROLL, diag=DIAG_ROLL, alpha = ALPHA_ROLL),
 }
 
-MODEL_DICT = {
+MESH_MODEL_DICT = {
   'cat': dict(label='Cat', view=400),
   'rat': dict(label='Rat', view=50)
 }
@@ -123,7 +122,6 @@ def WPC_initializeFigure(nb_rows=1, nb_col=1, share_x='all', share_y='all', opti
     new_pil_image = IMG_WPC_PIL.resize((int(Pil_width//3), int(Pil_height//3)))
     fig.figimage(new_pil_image, xo=fig.bbox.xmin, yo=fig.bbox.ymin, alpha=0.8, zorder = 0)
 
-    ## fig.figimage(IMG_WPC, xo=fig.bbox.xmin, yo=fig.bbox.ymin, alpha=1)
     ax_dict = {
       'main': ax_main,
       'plane_yaw': ax_plane_yaw,
@@ -159,27 +157,27 @@ def WPC_initializeFigure(nb_rows=1, nb_col=1, share_x='all', share_y='all', opti
 def WPC_initialize_plane(angle_type, fig, ax):
   ## Load image
   ax.set_axis_off()
-  d_plane = PLANE_DICT[angle_type]
-  img_plane = d_plane['img']
-  im = ax.imshow(img_plane)
+  type_plane = PLANE_DICT[angle_type]
+  plane_image = type_plane['img']
+  im = ax.imshow(plane_image)
 
-  size_plane = d_plane['size']
-  center_plane = d_plane['center']
-  diag_plane = d_plane['diag']
-  alpha_plane = d_plane['alpha']
-  tt = 1.15 ## Tolerance threshold of 20 percent to not have cut circle
+  size_plane_image = type_plane['size']
+  center_plane_image = type_plane['center']
+  diag_plane_image = type_plane['diag']
+  alpha_plane_image = type_plane['alpha']
+  tolerance = 1.15 ## Tolerance threshold of 20 percent to not have cut circle
 
-  ax.set_xlim(-alpha_plane[1]*tt, (size_plane[1] + alpha_plane[1])*tt)
-  ax.set_ylim(-alpha_plane[0]*tt, (size_plane[0] + alpha_plane[0])*tt)
+  ax.set_xlim(-alpha_plane_image[1]*tolerance, (size_plane_image[1] + alpha_plane_image[1])*tolerance)
+  ax.set_ylim(-alpha_plane_image[0]*tolerance, (size_plane_image[0] + alpha_plane_image[0])*tolerance)
 
-  ## display circle
-  cc = plt.Circle((center_plane[1], center_plane[0]), diag_plane/2, fill=False, color='black', linewidth=2, linestyle='dashdot')
-  ax.add_artist(cc)
+  ## Display cicle
+  circle_around_plane = plt.Circle((center_plane_image[1], center_plane_image[0]), diag_plane_image/2, fill=False, color='black', linewidth=2, linestyle='dashdot')
+  ax.add_artist(circle_around_plane)
   return  im
 
 def WPC_showFigure(fig):
-  w, h = fig.get_size_inches()
-  fig.set_size_inches(w, h, forward=True)
+  width, height = fig.get_size_inches()
+  fig.set_size_inches(width, height, forward=True)
   plt.ion() ## Turn on interactive mode
   plt.show()
   return
@@ -197,30 +195,30 @@ def WPC_getRotMat(roll, pitch, yaw, use_deg=False):
 
 def WPC_showEmpty(tag=DEFAULT_MODEL, save=0):
   fig, ax_dict = WPC_initializeFigure(option='3D')
-  im_yaw = WPC_initialize_plane('yaw', fig, ax_dict.get('plane_yaw'))
-  im_pitch = WPC_initialize_plane('pitch', fig, ax_dict.get('plane_pitch'))
-  im_roll = WPC_initialize_plane('roll', fig, ax_dict.get('plane_roll'))
+  yaw_im = WPC_initialize_plane('yaw', fig, ax_dict.get('plane_yaw'))
+  pitch_im = WPC_initialize_plane('pitch', fig, ax_dict.get('plane_pitch'))
+  roll_im = WPC_initialize_plane('roll', fig, ax_dict.get('plane_roll'))
 
   im_dict = {
-    'yaw': im_yaw,
-    'pitch': im_pitch,
-    'roll': im_roll,
+    'yaw': yaw_im,
+    'pitch': pitch_im,
+    'roll': roll_im,
   }
 
-  d = MODEL_DICT[tag] #model dict dictionary
+  mesh_model_type = MESH_MODEL_DICT[tag] #model dict dictionary
 
   ## Load
-  model = mesh.Mesh.from_file(f'{DATA_PATH}{tag}.stl')
-  data_orig = model.vectors ## 3D array
+  mesh_model = mesh.Mesh.from_file(f'{DATA_PATH}{tag}.stl')
+  data_orig = mesh_model.vectors ## 3D array
 
   ## Plot
-  poly = mplot3d.art3d.Poly3DCollection([], color=BMH_BLUE, edgecolor='k', lw=0.2) #or lightsteelblue
-  ax_dict.get('main').add_collection3d(poly)
+  mesh_poly = mplot3d.art3d.Poly3DCollection([], color=BMH_BLUE, edgecolor='k', lw=0.2) #or lightsteelblue
+  ax_dict.get('main').add_collection3d(mesh_poly)
 
   ## Settings
-  scale = [-0.6*d['view'], 0.6*d['view']]
+  mesh_scale = [-0.6*mesh_model_type['view'], 0.6*mesh_model_type['view']]
   ax_dict.get('main').view_init(elev=0, azim=0, roll=0)
-  ax_dict.get('main').auto_scale_xyz(scale, scale, scale)
+  ax_dict.get('main').auto_scale_xyz(mesh_scale, mesh_scale, mesh_scale)
   ax_dict.get('main').set_axis_off()
   ax_dict.get('main').set_title(f'Sensor fusion {tag}', weight='bold')
 
@@ -230,15 +228,15 @@ def WPC_showEmpty(tag=DEFAULT_MODEL, save=0):
   fig.canvas.manager.set_window_title('WPC AHRS visualization')
 
   WPC_showFigure(fig)
-  return fig, ax_dict, im_dict, data_orig, poly
+  return fig, ax_dict, im_dict, data_orig, mesh_poly
 
-def WPC_draw3DModel(fig, ax, data_orig, poly, roll, pitch, yaw):
+def WPC_draw3DModel(fig, ax, data_orig, mesh_poly, roll, pitch, yaw):
   ## Rotate
   rot_mat = WPC_getRotMat(roll, pitch, yaw, use_deg=True)
-  data = data_orig.dot(rot_mat)
+  mesh_data = data_orig.dot(rot_mat)
 
   ## Plot
-  poly.set_verts(data)
+  mesh_poly.set_verts(mesh_data)
   return
 
 def WPC_plot_plane(fig, ax, angle_type, im, center):
@@ -278,11 +276,11 @@ def main():
     dev = pywpc.WifiDAQE3AH()
 
     ## Show empty
-    fig, ax_dict, im_dict, data_orig, poly = WPC_showEmpty()
+    fig, ax_dict, im_dict, data_orig, mesh_poly = WPC_showEmpty()
 
     ## Connect to device
     try:
-      dev.connect("192.168.5.38") ## Depend on your device
+      dev.connect("192.168.5.39") ## Depend on your device
     except Exception as err:
       pywpc.printGenericError(err)
       ## Release device handle
@@ -293,7 +291,7 @@ def main():
       ## Parameters setting
       port = 0 ## Depend on your device
       sampling_period = 0.003
-      mode = 0 ## 0: Orientation, 1: Acceleration, 2: Orientation + Acceleration
+      mode = 0 ## 0: Orientation, 1: Acircle_around_planeeleration, 2: Orientation + Acircle_around_planeeleration
       timeout = 3 ## second
 
       ## Get firmware model & version
@@ -316,7 +314,7 @@ def main():
       while plt.fignum_exists(fig.number):
           ahrs_list = dev.AHRS_getEstimate(port, mode, timeout)
           if len(ahrs_list) > 0:
-              WPC_draw3DModel(fig, ax_dict.get('main'), data_orig, poly, ahrs_list[0], ahrs_list[1], ahrs_list[2])
+              WPC_draw3DModel(fig, ax_dict.get('main'), data_orig, mesh_poly, ahrs_list[0], ahrs_list[1], ahrs_list[2])
               WPC_plot_plane(fig, ax_dict.get('plane_roll'), ahrs_list[0], im_dict.get('roll'), CENTER_ROLL)
               WPC_plot_plane(fig, ax_dict.get('plane_pitch'), ahrs_list[1], im_dict.get('pitch'), CENTER_PITCH)
               WPC_plot_plane(fig, ax_dict.get('plane_yaw'), ahrs_list[2], im_dict.get('yaw'), CENTER_YAW)
